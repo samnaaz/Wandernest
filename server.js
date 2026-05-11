042fefd00f64398cd842266d0aabe88b2960f128
KISHAN1717 <kishanshetty1717@gmail.com>
2026-05-11 15:29:45 +0530

    third commit

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const app = require("../app");
-const { port } = require("./config/env");
+const app = require("./app");
+const { port } = require("./src/config/env");
 
 app.listen(port, () => {
   console.log(`WanderNest backend listening on port ${port}`);
